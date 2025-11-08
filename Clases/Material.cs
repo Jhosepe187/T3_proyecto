a5e4cc27d1d34f671eda937a0aa9184e86278c80
Jhosep187 <jhosepbva9@gmail.com>
2025-11-08 15:08:52 -0500

    Agregar tipo de Material

---
+++ Clases/Material.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Material
+    {
+        string tipo;
+        int Peso;
+        string proveniencia;
+
+        public Material(string tipo, int peso, string proveniencia)
+        {
+            this.tipo = tipo;
+            Peso = peso;
+            this.proveniencia = proveniencia;
+        }
+    }
+}
