42eb3475f828f3b447baedd15b3bd5723fa4c031
Jhosep187 <jhosepbva9@gmail.com>
2025-11-08 14:57:00 -0500

    Eliminar pruebas

--- Ejecucion/Program.cs
+++ Ejecucion/Program.cs
@@ -10,9 +10,7 @@ namespace Ejecucion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HOLA MUNDO");
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ReadLine();
+            
         }
     }
 }
