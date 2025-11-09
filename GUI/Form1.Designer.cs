d6a8701e2c147f0fe3bbca34103a789241abe68e
Jhosep187 <jhosepbva9@gmail.com>
2025-11-08 20:51:39 -0500

    Avanzando diseño-logica

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -29,68 +29,147 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.btton_RegistrarMaterial = new System.Windows.Forms.Button();
+            this.bttn_MostrarMaterialesClasificados = new System.Windows.Forms.Button();
+            this.bttn_MostrarHistorial = new System.Windows.Forms.Button();
+            this.bttn_MostrarColaDeMateriales = new System.Windows.Forms.Button();
+            this.bttn_ProcesarMateriales = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_Resgitrar1 = new System.Windows.Forms.Label();
+            this.txtBox_TipoMaterial = new System.Windows.Forms.TextBox();
+            this.grpRegistrar1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.grpRegistrar1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(401, 23);
+            this.label1.Font = new System.Drawing.Font("Poor Richard", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Location = new System.Drawing.Point(244, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.Size = new System.Drawing.Size(280, 36);
             this.label1.TabIndex = 0;
             this.label1.Text = "SISTEMA DE RECICLAJE";
             // 
-            // label2
+            // btton_RegistrarMaterial
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Registrar material";
+            this.btton_RegistrarMaterial.Location = new System.Drawing.Point(6, 29);
+            this.btton_RegistrarMaterial.Name = "btton_RegistrarMaterial";
+            this.btton_RegistrarMaterial.Size = new System.Drawing.Size(178, 36);
+            this.btton_RegistrarMaterial.TabIndex = 1;
+            this.btton_RegistrarMaterial.Text = "Registrar Material";
+            this.btton_RegistrarMaterial.UseVisualStyleBackColor = true;
+            this.btton_RegistrarMaterial.Click += new System.EventHandler(this.btton_RegistrarMaterial_Click);
             // 
-            // label3
+            // bttn_MostrarMaterialesClasificados
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(78, 160);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Mostrar cola de materiales";
+            this.bttn_MostrarMaterialesClasificados.Location = new System.Drawing.Point(6, 183);
+            this.bttn_MostrarMaterialesClasificados.Name = "bttn_MostrarMaterialesClasificados";
+            this.bttn_MostrarMaterialesClasificados.Size = new System.Drawing.Size(178, 36);
+            this.bttn_MostrarMaterialesClasificados.TabIndex = 2;
+            this.bttn_MostrarMaterialesClasificados.Text = "Mostrar Materiales Clasificados";
+            this.bttn_MostrarMaterialesClasificados.UseVisualStyleBackColor = true;
             // 
-            // label4
+            // bttn_MostrarHistorial
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(78, 218);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Procesor";
+            this.bttn_MostrarHistorial.Location = new System.Drawing.Point(6, 234);
+            this.bttn_MostrarHistorial.Name = "bttn_MostrarHistorial";
+            this.bttn_MostrarHistorial.Size = new System.Drawing.Size(178, 36);
+            this.bttn_MostrarHistorial.TabIndex = 3;
+            this.bttn_MostrarHistorial.Text = "Mostrar Historial";
+            this.bttn_MostrarHistorial.UseVisualStyleBackColor = true;
+            // 
+            // bttn_MostrarColaDeMateriales
+            // 
+            this.bttn_MostrarColaDeMateriales.Location = new System.Drawing.Point(6, 132);
+            this.bttn_MostrarColaDeMateriales.Name = "bttn_MostrarColaDeMateriales";
+            this.bttn_MostrarColaDeMateriales.Size = new System.Drawing.Size(178, 36);
+            this.bttn_MostrarColaDeMateriales.TabIndex = 4;
+            this.bttn_MostrarColaDeMateriales.Text = "Mostrar Cola de Materiales";
+            this.bttn_MostrarColaDeMateriales.UseVisualStyleBackColor = true;
+            // 
+            // bttn_ProcesarMateriales
+            // 
+            this.bttn_ProcesarMateriales.Location = new System.Drawing.Point(6, 80);
+            this.bttn_ProcesarMateriales.Name = "bttn_ProcesarMateriales";
+            this.bttn_ProcesarMateriales.Size = new System.Drawing.Size(178, 36);
+            this.bttn_ProcesarMateriales.TabIndex = 5;
+            this.bttn_ProcesarMateriales.Text = "Procesar materiales";
+            this.bttn_ProcesarMateriales.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btton_RegistrarMaterial);
+            this.groupBox1.Controls.Add(this.bttn_MostrarHistorial);
+            this.groupBox1.Controls.Add(this.bttn_MostrarColaDeMateriales);
+            this.groupBox1.Controls.Add(this.bttn_ProcesarMateriales);
+            this.groupBox1.Controls.Add(this.bttn_MostrarMaterialesClasificados);
+            this.groupBox1.Location = new System.Drawing.Point(42, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 286);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "OPCIONES";
+            // 
+            // lbl_Resgitrar1
+            // 
+            this.lbl_Resgitrar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.lbl_Resgitrar1.Location = new System.Drawing.Point(6, 16);
+            this.lbl_Resgitrar1.Name = "lbl_Resgitrar1";
+            this.lbl_Resgitrar1.Size = new System.Drawing.Size(342, 212);
+            this.lbl_Resgitrar1.TabIndex = 7;
+            this.lbl_Resgitrar1.Text = "                        REGISTRAR NUEVO MATERIAL\r\n Tipos disponibles:\r\n\r\n1. Plást" +
+    "ico\r\n\r\n2. Vidrio\r\n\r\n3. Papel\r\n\r\n4. Metal\r\n\r\nIngrese el tipo de Material (1-4)\r\n";
+            // 
+            // txtBox_TipoMaterial
+            // 
+            this.txtBox_TipoMaterial.Location = new System.Drawing.Point(9, 234);
+            this.txtBox_TipoMaterial.Name = "txtBox_TipoMaterial";
+            this.txtBox_TipoMaterial.Size = new System.Drawing.Size(68, 20);
+            this.txtBox_TipoMaterial.TabIndex = 8;
+            // 
+            // grpRegistrar1
+            // 
+            this.grpRegistrar1.Controls.Add(this.lbl_Resgitrar1);
+            this.grpRegistrar1.Controls.Add(this.txtBox_TipoMaterial);
+            this.grpRegistrar1.Location = new System.Drawing.Point(288, 65);
+            this.grpRegistrar1.Name = "grpRegistrar1";
+            this.grpRegistrar1.Size = new System.Drawing.Size(385, 283);
+            this.grpRegistrar1.TabIndex = 9;
+            this.grpRegistrar1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(977, 587);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(757, 389);
+            this.Controls.Add(this.grpRegistrar1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.grpRegistrar1.ResumeLayout(false);
+            this.grpRegistrar1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btton_RegistrarMaterial;
+        private System.Windows.Forms.Button bttn_MostrarMaterialesClasificados;
+        private System.Windows.Forms.Button bttn_MostrarHistorial;
+        private System.Windows.Forms.Button bttn_MostrarColaDeMateriales;
+        private System.Windows.Forms.Button bttn_ProcesarMateriales;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_Resgitrar1;
+        private System.Windows.Forms.TextBox txtBox_TipoMaterial;
+        private System.Windows.Forms.GroupBox grpRegistrar1;
     }
 }
 
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,55 @@ namespace GUI
 {
     public partial class Form1 : Form
     {
+        Cola cola = new Cola();
+        Arbol Arbol = new Arbol();
+        Pila Pila = new Pila(); 
         public Form1()
         {
             InitializeComponent();
+            grpRegistrar1.Visible = false;
+
+        }
+
+        private void btton_RegistrarMaterial_Click(object sender, EventArgs e)
+        {
+            grpRegistrar1.Visible = true;
+            int tipoNum = int.Parse(txtBox_TipoMaterial.Text);
+            if (!int.TryParse(txtBox_TipoMaterial.Text, out tipoNum) || tipoNum < 1 || tipoNum > 4)
+            {
+                MessageBox.Show("Tipo inválido. Operación cancelada.");
+                return;
+            }
+
+            string tipoTexto = "";
+            switch (tipoNum)
+            {
+                case 1:
+                    tipoTexto = "Plástico";
+                    break;
+                case 2:
+                    tipoTexto = "Vidrio";
+                    break;
+                case 3:
+                    tipoTexto = "Papel";
+                    break;
+                case 4:
+                    tipoTexto = "Metal";
+                    break;
+                default:
+                    tipoTexto = "Desconocido";
+                    break;
+            }
+            grpRegistrar1.Visible = false;
+
+            Console.Write("Ingrese el nombre del material: ");
+            string nombre = Console.ReadLine();
+
+            Material nuevo = new Material(tipoTexto, nombre);
+            cola.Encolar(nuevo);
+
+            Console.WriteLine($"Material '{nombre}' de tipo '{tipoTexto}' registrado correctamente.");
+
         }
 
        
