18b474d8b4c3963efc09f229431523e0596e2791
Jhosep187 <jhosepbva9@gmail.com>
2025-11-09 13:00:03 -0500

    VERSION HECHA COMPLETA

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -28,49 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.btnRegistrarMaterial = new System.Windows.Forms.Button();
             this.btnClasificados = new System.Windows.Forms.Button();
             this.btnHistorial = new System.Windows.Forms.Button();
             this.btnMostrarCola = new System.Windows.Forms.Button();
             this.btnProcesar = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lbl_Resgitrar1 = new System.Windows.Forms.Label();
             this.txtTipo = new System.Windows.Forms.TextBox();
             this.grpRegistrar = new System.Windows.Forms.GroupBox();
-            this.txtSalida = new System.Windows.Forms.TextBox();
             this.btnRegistrar = new System.Windows.Forms.Button();
             this.txtNombre = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
+            this.txtSalida = new System.Windows.Forms.TextBox();
             this.grpRegistrar.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Poor Richard", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
-            this.label1.Location = new System.Drawing.Point(244, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(280, 36);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "SISTEMA DE RECICLAJE";
-            // 
             // btnRegistrarMaterial
             // 
-            this.btnRegistrarMaterial.Location = new System.Drawing.Point(6, 29);
+            this.btnRegistrarMaterial.BackColor = System.Drawing.Color.Transparent;
+            this.btnRegistrarMaterial.BackgroundImage = global::GUI.Properties.Resources.Texto__1_;
+            this.btnRegistrarMaterial.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnRegistrarMaterial.FlatAppearance.BorderSize = 0;
+            this.btnRegistrarMaterial.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnRegistrarMaterial.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnRegistrarMaterial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrarMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrarMaterial.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRegistrarMaterial.Location = new System.Drawing.Point(270, 413);
             this.btnRegistrarMaterial.Name = "btnRegistrarMaterial";
-            this.btnRegistrarMaterial.Size = new System.Drawing.Size(178, 36);
+            this.btnRegistrarMaterial.Size = new System.Drawing.Size(118, 34);
             this.btnRegistrarMaterial.TabIndex = 1;
             this.btnRegistrarMaterial.Text = "Registrar Material";
-            this.btnRegistrarMaterial.UseVisualStyleBackColor = true;
+            this.btnRegistrarMaterial.UseVisualStyleBackColor = false;
             this.btnRegistrarMaterial.Click += new System.EventHandler(this.btnRegistrarMaterial_Click);
             // 
             // btnClasificados
             // 
-            this.btnClasificados.Location = new System.Drawing.Point(6, 183);
+            this.btnClasificados.BackgroundImage = global::GUI.Properties.Resources.Texto__1_;
+            this.btnClasificados.FlatAppearance.BorderSize = 0;
+            this.btnClasificados.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClasificados.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClasificados.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClasificados.Location = new System.Drawing.Point(340, 470);
             this.btnClasificados.Name = "btnClasificados";
-            this.btnClasificados.Size = new System.Drawing.Size(178, 36);
+            this.btnClasificados.Size = new System.Drawing.Size(119, 34);
             this.btnClasificados.TabIndex = 2;
             this.btnClasificados.Text = "Mostrar Materiales Clasificados";
             this.btnClasificados.UseVisualStyleBackColor = true;
@@ -78,9 +78,14 @@
             // 
             // btnHistorial
             // 
-            this.btnHistorial.Location = new System.Drawing.Point(6, 234);
+            this.btnHistorial.BackgroundImage = global::GUI.Properties.Resources.Texto__1_;
+            this.btnHistorial.FlatAppearance.BorderSize = 0;
+            this.btnHistorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHistorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHistorial.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnHistorial.Location = new System.Drawing.Point(515, 470);
             this.btnHistorial.Name = "btnHistorial";
-            this.btnHistorial.Size = new System.Drawing.Size(178, 36);
+            this.btnHistorial.Size = new System.Drawing.Size(112, 29);
             this.btnHistorial.TabIndex = 3;
             this.btnHistorial.Text = "Mostrar Historial";
             this.btnHistorial.UseVisualStyleBackColor = true;
@@ -88,9 +93,14 @@
             // 
             // btnMostrarCola
             // 
-            this.btnMostrarCola.Location = new System.Drawing.Point(6, 132);
+            this.btnMostrarCola.BackgroundImage = global::GUI.Properties.Resources.Texto__1_;
+            this.btnMostrarCola.FlatAppearance.BorderSize = 0;
+            this.btnMostrarCola.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrarCola.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarCola.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMostrarCola.Location = new System.Drawing.Point(586, 413);
             this.btnMostrarCola.Name = "btnMostrarCola";
-            this.btnMostrarCola.Size = new System.Drawing.Size(178, 36);
+            this.btnMostrarCola.Size = new System.Drawing.Size(122, 34);
             this.btnMostrarCola.TabIndex = 4;
             this.btnMostrarCola.Text = "Mostrar Cola de Materiales";
             this.btnMostrarCola.UseVisualStyleBackColor = true;
@@ -98,32 +108,27 @@
             // 
             // btnProcesar
             // 
-            this.btnProcesar.Location = new System.Drawing.Point(6, 80);
+            this.btnProcesar.BackColor = System.Drawing.Color.Transparent;
+            this.btnProcesar.BackgroundImage = global::GUI.Properties.Resources.Texto__1_;
+            this.btnProcesar.FlatAppearance.BorderSize = 0;
+            this.btnProcesar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnProcesar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnProcesar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnProcesar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnProcesar.Location = new System.Drawing.Point(423, 415);
             this.btnProcesar.Name = "btnProcesar";
-            this.btnProcesar.Size = new System.Drawing.Size(178, 36);
+            this.btnProcesar.Size = new System.Drawing.Size(127, 29);
             this.btnProcesar.TabIndex = 5;
             this.btnProcesar.Text = "Procesar materiales";
-            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.UseVisualStyleBackColor = false;
             this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.btnRegistrarMaterial);
-            this.groupBox1.Controls.Add(this.btnHistorial);
-            this.groupBox1.Controls.Add(this.btnMostrarCola);
-            this.groupBox1.Controls.Add(this.btnProcesar);
-            this.groupBox1.Controls.Add(this.btnClasificados);
-            this.groupBox1.Location = new System.Drawing.Point(42, 62);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(190, 286);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "OPCIONES";
             // 
             // lbl_Resgitrar1
             // 
             this.lbl_Resgitrar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.lbl_Resgitrar1.Location = new System.Drawing.Point(6, 26);
+            this.lbl_Resgitrar1.Location = new System.Drawing.Point(6, 29);
             this.lbl_Resgitrar1.Name = "lbl_Resgitrar1";
             this.lbl_Resgitrar1.Size = new System.Drawing.Size(211, 212);
             this.lbl_Resgitrar1.TabIndex = 7;
@@ -139,35 +144,34 @@
             // 
             // grpRegistrar
             // 
-            this.grpRegistrar.Controls.Add(this.txtSalida);
+            this.grpRegistrar.BackColor = System.Drawing.Color.Transparent;
+            this.grpRegistrar.BackgroundImage = global::GUI.Properties.Resources.Texto__1_;
             this.grpRegistrar.Controls.Add(this.btnRegistrar);
             this.grpRegistrar.Controls.Add(this.txtNombre);
             this.grpRegistrar.Controls.Add(this.lbl_Resgitrar1);
             this.grpRegistrar.Controls.Add(this.txtTipo);
-            this.grpRegistrar.Location = new System.Drawing.Point(249, 65);
+            this.grpRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grpRegistrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.grpRegistrar.Location = new System.Drawing.Point(299, 91);
             this.grpRegistrar.Name = "grpRegistrar";
-            this.grpRegistrar.Size = new System.Drawing.Size(385, 283);
+            this.grpRegistrar.Size = new System.Drawing.Size(385, 274);
             this.grpRegistrar.TabIndex = 9;
             this.grpRegistrar.TabStop = false;
             // 
-            // txtSalida
-            // 
-            this.txtSalida.Location = new System.Drawing.Point(0, -3);
-            this.txtSalida.Multiline = true;
-            this.txtSalida.Name = "txtSalida";
-            this.txtSalida.ReadOnly = true;
-            this.txtSalida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtSalida.Size = new System.Drawing.Size(385, 283);
-            this.txtSalida.TabIndex = 10;
-            // 
             // btnRegistrar
             // 
-            this.btnRegistrar.Location = new System.Drawing.Point(141, 228);
+            this.btnRegistrar.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btnRegistrar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnRegistrar.FlatAppearance.BorderSize = 0;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnRegistrar.Location = new System.Drawing.Point(131, 220);
             this.btnRegistrar.Name = "btnRegistrar";
             this.btnRegistrar.Size = new System.Drawing.Size(117, 38);
             this.btnRegistrar.TabIndex = 10;
             this.btnRegistrar.Text = "Confirmar";
-            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.UseVisualStyleBackColor = false;
             this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
             // txtNombre
@@ -177,33 +181,50 @@
             this.txtNombre.Size = new System.Drawing.Size(123, 20);
             this.txtNombre.TabIndex = 9;
             // 
+            // txtSalida
+            // 
+            this.txtSalida.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtSalida.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSalida.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalida.Location = new System.Drawing.Point(270, 101);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.ReadOnly = true;
+            this.txtSalida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSalida.Size = new System.Drawing.Size(438, 231);
+            this.txtSalida.TabIndex = 10;
+            this.txtSalida.TextChanged += new System.EventHandler(this.txtSalida_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(708, 389);
+            this.BackgroundImage = global::GUI.Properties.Resources.Texto__2_;
+            this.ClientSize = new System.Drawing.Size(968, 583);
+            this.Controls.Add(this.btnHistorial);
+            this.Controls.Add(this.btnRegistrarMaterial);
+            this.Controls.Add(this.btnClasificados);
+            this.Controls.Add(this.btnMostrarCola);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.btnProcesar);
             this.Controls.Add(this.grpRegistrar);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
             this.grpRegistrar.ResumeLayout(false);
             this.grpRegistrar.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnRegistrarMaterial;
         private System.Windows.Forms.Button btnClasificados;
         private System.Windows.Forms.Button btnHistorial;
         private System.Windows.Forms.Button btnMostrarCola;
         private System.Windows.Forms.Button btnProcesar;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label lbl_Resgitrar1;
         private System.Windows.Forms.TextBox txtTipo;
         private System.Windows.Forms.GroupBox grpRegistrar;
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -14,147 +14,143 @@ namespace GUI
     public partial class Form1 : Form
     {
         Cola cola = new Cola();
-        Arbol Arbol = new Arbol();
-        Pila Pila = new Pila();
+        Arbol arbol = new Arbol();
+        Pila pila = new Pila();
+
         public Form1()
         {
             InitializeComponent();
             grpRegistrar.Visible = false;
-            //txtSalida.Visible = false;
+            txtSalida.Visible = false; // Inicialmente oculto
         }
 
         private void btnRegistrarMaterial_Click(object sender, EventArgs e)
         {
             grpRegistrar.Visible = true;
             txtSalida.Visible = false;
-
         }
 
-        private void btnRegistrar_Click(object sender, EventArgs e)
+        private void btnProcesar_Click(object sender, EventArgs e)
         {
-
-            int tipoNum;
-            if (!int.TryParse(txtTipo.Text, out tipoNum) || tipoNum < 1 || tipoNum > 4)
+            txtSalida.Visible = true; // Hacer visible el TextBox
+            txtSalida.Text = ""; // Limpiar el texto antes de procesar y mostrar
+            if (cola.EsVacio())
             {
-                MessageBox.Show("Ingrese un número válido (1-4).");
+                txtSalida.Text = "No hay materiales en la cola para procesar.";
                 return;
             }
-
-            string tipoTexto = "";
-            switch (tipoNum)
+            while (!cola.EsVacio())
             {
-                case 1:
-                    tipoTexto = "Plástico";
-                    break;
-                case 2:
-                    tipoTexto = "Vidrio";
-                    break;
-                case 3:
-                    tipoTexto = "Papel";
-                    break;
-                case 4:
-                    tipoTexto = "Metal";
-                    break;
-                default:
-                    tipoTexto = "Desconocido";
-                    break;
+                Material m = cola.Desencolar();
+                arbol.Insertar(m);
+                pila.Apilar(m);
+                txtSalida.AppendText($"Procesado: {m}\r\n");
             }
-
-            string nombre = txtNombre.Text.Trim();
-            if (string.IsNullOrEmpty(nombre))
-            {
-                MessageBox.Show("Ingrese un nombre para el material.");
-                return;
-            }
-
-            Material nuevo = new Material(tipoTexto, nombre);
-            cola.Encolar(nuevo);
-
-            txtSalida.Visible = true;
-            txtSalida.Text = $"✅ Material '{nombre}' de tipo '{tipoTexto}' registrado correctamente.";
-            grpRegistrar.Visible = false;
-            txtTipo.Clear();
-            txtNombre.Clear();
-
+            txtSalida.AppendText("\nTodos los materiales han sido clasificados y guardados en el historial.");
         }
 
         private void btnMostrarCola_Click(object sender, EventArgs e)
         {
-          
-
+            txtSalida.Visible = true; // Hacer visible el TextBox
+            txtSalida.Text = ""; // Limpiar el texto antes de mostrar
             if (cola.EsVacio())
             {
                 txtSalida.Text = "No hay materiales en la cola.";
                 return;
             }
-
             Nodo aux = cola.frente;
             while (aux != null)
             {
                 txtSalida.AppendText(aux.dato.ToString() + Environment.NewLine);
                 aux = aux.sig;
             }
-           
-        }
-
-        private void btnProcesar_Click(object sender, EventArgs e)
-        {
-            
-            if (cola.EsVacio())
-            {
-                txtSalida.Text = "No hay materiales en la cola para procesar.";
-                return;
-            }
-
-          
-            while (!cola.EsVacio())
-            {
-                Material m = cola.Desencolar();
-                Arbol.Insertar(m);
-                Pila.Apilar(m);
-                txtSalida.AppendText($"Procesado: {m}\r\n");
-            }
-
-            txtSalida.AppendText("\nTodos los materiales han sido clasificados y guardados en el historial.");
-          
         }
 
         private void btnClasificados_Click(object sender, EventArgs e)
         {
-   
-            MostrarArbolEnOrden(Arbol.raiz_principal);
+            txtSalida.Visible = true; // Hacer visible el TextBox
+            txtSalida.Text = ""; // Limpiar el texto antes de mostrar
+            if (arbol.raiz_principal == null)
+            {
+                txtSalida.Text = "No hay materiales clasificados.";
+                return;
+            }
+            MostrarArbolEnOrden(arbol.raiz_principal);
         }
 
         private void MostrarArbolEnOrden(Nodo raiz)
         {
-            
-          
             if (raiz != null)
             {
                 MostrarArbolEnOrden(raiz.izq);
                 txtSalida.AppendText(raiz.dato.ToString() + Environment.NewLine);
                 MostrarArbolEnOrden(raiz.der);
             }
-          
         }
 
         private void btnHistorial_Click(object sender, EventArgs e)
         {
-
-           
-            Nodo aux = Pila.cima;
+            txtSalida.Visible = true; // Hacer visible el TextBox
+            txtSalida.Text = ""; // Limpiar el texto antes de mostrar
+            Nodo aux = pila.cima;
             if (aux == null)
             {
                 txtSalida.Text = "No hay historial disponible.";
                 return;
             }
-
             while (aux != null)
             {
                 txtSalida.AppendText(aux.dato.ToString() + Environment.NewLine);
                 aux = aux.sig;
             }
-         
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            int tipoNum;
+            if (!int.TryParse(txtTipo.Text, out tipoNum) || tipoNum < 1 || tipoNum > 4)
+            {
+                MessageBox.Show("Ingrese un número válido (1-4).");
+                return;
+            }
+                string tipoTexto = "";
+                switch (tipoNum)
+                {
+                    case 1:
+                        tipoTexto = "Plástico";
+                        break;
+                    case 2:
+                        tipoTexto = "Vidrio";
+                        break;
+                    case 3:
+                        tipoTexto = "Papel";
+                        break;
+                    case 4:
+                        tipoTexto = "Metal";
+                        break;
+                    default:
+                        tipoTexto = "Desconocido";
+                        break;
+                }
+                string nombre = txtNombre.Text.Trim();
+                if (string.IsNullOrEmpty(nombre))
+                {
+                    MessageBox.Show("Ingrese un nombre para el material.");
+                    return;
+                }
+                Material nuevo = new Material(tipoTexto, nombre);
+                cola.Encolar(nuevo);
+                txtSalida.Visible = true;
+                txtSalida.Text = $"Material '{nombre}' de tipo '{tipoTexto}' registrado correctamente.";
+                grpRegistrar.Visible = false;
+                txtTipo.Clear();
+                txtNombre.Clear();
+            }
+
+        private void txtSalida_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
-}
+    }
+
